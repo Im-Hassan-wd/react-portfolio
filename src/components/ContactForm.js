7c2e23c272c46ff1ed56eb1a9efe2803c39d7f9e
huzitaiwo <alhassansahih@gmail.com>
2023-06-03 20:45:35 +0100

    pages and components

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,17 +2,40 @@ import "./ContactForm.css";
 
 export default function ContactForm() {
   return (
-    <div className="contact-form">
+    <section className="contact">
       <div className="form-intro">
         <div className="form-header">
-          Let's connect.<i className="fi fi-bs-arrow-small-right"></i>
+          Let's<i className="fi fi-bs-arrow-small-right"></i> connect.
         </div>
         <p>
           Warwick is a skilled web designer and developer ready to bring your
           online vision to life - contact me today to discuss your project and
           create a captivating website that exceeds your expectations.
         </p>
+        <h1 className="heading">Inquiries</h1>
       </div>
-    </div>
+
+      <form>
+        <label>
+          <span>Your name:</span>
+          <input type="text" required />
+        </label>
+
+        <label>
+          <span>Your email address:</span>
+          <input type="email" required />
+        </label>
+
+        <label>
+          <span>Tell us about the project:</span>
+          <textarea required style={{ height: "max-content" }} />
+        </label>
+
+        <button>
+          <span>Send</span>
+          <i className="fi fi-bs-arrow-small-right"></i>
+        </button>
+      </form>
+    </section>
   );
 }
